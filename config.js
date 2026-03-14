d4275134f6cc35d0b850b80b7c58299e545d7565
Basmali12 <163114165+Basmali12@users.noreply.github.com>
2026-03-15 00:21:42 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+window.MY_STORE_CONFIG = {
+  firebase: {
+    apiKey: "AIzaSyAEX8BjlYjsHOxDwD-Wu9qyFyIR3Wb6RxQ",
+    authDomain: "mtgr-24718.firebaseapp.com",
+    databaseURL: "https://mtgr-24718-default-rtdb.firebaseio.com",
+    projectId: "mtgr-24718",
+    storageBucket: "mtgr-24718.firebasestorage.app",
+    messagingSenderId: "385919783070",
+    appId: "1:385919783070:web:64f2703ec8cc0fd1c5ab8f"
+  },
+  security: {
+    adminCode: "1001"
+  }
+};
