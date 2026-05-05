f361a71e5d72e7294902791a3ff460809b9fc820
Basmali12 <163114165+Basmali12@users.noreply.github.com>
2026-05-05 12:52:40 +0300

    Refactor config.js by removing comments
    Removed comments and cleaned up the config structure.

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-// قسم المتغيرات الأساسية للمشروع
 const CONFIG = {
-  // مفتاح API الخاص بموقع ImgBB لرفع الصور
   IMGBB_API_KEY: "7765600ba5d52e397d9eb90496cc46c6",
-  
-  // إعدادات قاعدة بيانات Firebase (يرجى وضع مفاتيح مشروعك هنا)
   FIREBASE_CONFIG: {
     apiKey: "YOUR_API_KEY",
     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
@@ -12,11 +8,8 @@ const CONFIG = {
     messagingSenderId: "YOUR_SENDER_ID",
     appId: "YOUR_APP_ID"
   },
-  
-  // اسم المجموعة التي ستحفظ فيها المنتجات داخل Firestore
   COLLECTION_NAME: "products"
 };
 
-// تهيئة الاتصال بقاعدة بيانات Firebase
 firebase.initializeApp(CONFIG.FIREBASE_CONFIG);
 const db = firebase.firestore();
