cb923d68af6cd5ba1d9438776e6ebe3872b1bc05
Basmali12 <163114165+Basmali12@users.noreply.github.com>
2026-05-05 12:45:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,22 @@
-// config.js
-window.MY_STORE_CONFIG = {
-  firebase: {
-    apiKey: "AIzaSyAEX8BjlYjsHOxDwD-Wu9qyFyIR3Wb6RxQ",
-    authDomain: "mtgr-24718.firebaseapp.com",
-    databaseURL: "https://mtgr-24718-default-rtdb.firebaseio.com",
-    projectId: "mtgr-24718",
-    storageBucket: "mtgr-24718.firebasestorage.app",
-    messagingSenderId: "385919783070",
-    appId: "1:385919783070:web:64f2703ec8cc0fd1c5ab8f"
+// قسم المتغيرات الأساسية للمشروع
+const CONFIG = {
+  // مفتاح API الخاص بموقع ImgBB لرفع الصور
+  IMGBB_API_KEY: "7765600ba5d52e397d9eb90496cc46c6",
+  
+  // إعدادات قاعدة بيانات Firebase (يرجى وضع مفاتيح مشروعك هنا)
+  FIREBASE_CONFIG: {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
   },
-  security: {
-    adminCode: "1001"
-  }
+  
+  // اسم المجموعة التي ستحفظ فيها المنتجات داخل Firestore
+  COLLECTION_NAME: "products"
 };
+
+// تهيئة الاتصال بقاعدة بيانات Firebase
+firebase.initializeApp(CONFIG.FIREBASE_CONFIG);
+const db = firebase.firestore();
